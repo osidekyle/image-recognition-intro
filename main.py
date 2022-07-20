ac54754fca99b317fa72754fd25ab0d155267b12
osidekyle <hawkinskyle67@gmail.com>
2022-07-19 23:13:33 -0700

    initial setup

---
+++ main.py
@@ -0,0 +1,10 @@
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+
+i = Image.open("images/numbers/y0.4.png")
+iar = np.asarray(i)
+
+plt.imshow(iar)
+print(iar)
+plt.show()
